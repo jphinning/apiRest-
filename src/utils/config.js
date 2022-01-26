d6c67812d29042a87dbd806ffcded30bc3b3fbbe
Joao Hinning <jphinning@gmail.com>
2022-01-26 17:10:16 -0300

    Added Procfile

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const { PORT } = process.env;
+const { PORT } = process.env || 3001;
 const MONGODB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@apirest.qckha.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 module.exports = {
